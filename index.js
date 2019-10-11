5ce726efae83c12152ceb53f9d769a4463b5b97d
Aakil Fernandes <aakilfernandes@gmail.com>
2019-10-11 14:24:15 -0400

    fix hashcash worker url

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const client = new pollenium.Client({
   friendsMax: 6,
   Worker: Worker,
   WebSocket: WebSocket,
-  hashcashWorkerUrl: '/protocol/browser/hashcash-worker.js'
+  hashcashWorkerUrl: './lib/hashcash-worker.js'
 })
 const applicationId = pollenium.Bytes.fromUtf8('pollenboard').getPaddedLeft(32)
 
