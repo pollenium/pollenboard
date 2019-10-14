52c3e3ad379e31dc071e54823ba9879ea6798d40
Aakil Fernandes <aakilfernandes@gmail.com>
2019-10-14 16:00:13 -0400

    upgrade pollenium-anemone

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const client = new pollenium.Client({
   friendsMax: 6,
   Worker: Worker,
   WebSocket: WebSocket,
-  hashcashWorkerUrl: './lib/pollenium/hashcash-worker.js'
+  hashcashWorkerUrl: './lib/pollenium-anemone/hashcash-worker.js'
 })
 const applicationId = pollenium.Bytes.fromUtf8('pollenboard').getPaddedLeft(32)
 
